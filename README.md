0dca0addd3d92d197a45764e24567b7a9ee92134
Matt Davis <matt@Matts-MacBook-Pro.local>
2021-08-13 17:10:02 +0300

    add exercises

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-#Sorting Exercises I
+# Sorting Algorithms Exercise
+
+For this exercise you must get the tests to pass.
+
+### Part I
+
+#### bubble sort
+
+Implement bubble sort. Your function should accept an array and return an array of sorted values.
+
+#### selection sort
+
+Implement selection sort. Your function should accept an array and return an array of sorted values.
+
+#### insertion sort
+
+Implement insertion sort. Your function should accept an array and return an array of sorted values.
---
+++ sortingSpec.js
@@ -0,0 +1,49 @@
+var expect = chai.expect;
+var unsorted, sorted, unsortedNegative, sortedNegative;
+
+beforeEach(function(){
+  unsorted = [4,5,1,21,2,9,10,15];
+  sorted = [1,2,4,5,9,10,15,21];
+  unsortedNegative = [9,-2,0,35,4,-10,22,12];
+  sortedNegative = [-10,-2,0,4,9,12,22,35];
+});
+
+describe("#bubbleSort", function(){
+
+  it("should sort an array of positive and negative numbers ", function(){
+    expect(bubbleSort(unsorted)).to.deep.equal(sorted);
+    expect(bubbleSort(unsortedNegative)).to.deep.equal(sortedNegative);
+  });
+
+  it("should return an empty array if passed an empty array", function(){
+    expect(bubbleSort([])).to.deep.equal([]);
+  });
+
+});
+
+describe("#selectionSort", function(){
+
+  it("should sort an array of positive and negative numbers ", function(){
+    expect(selectionSort(unsorted)).to.deep.equal(sorted);
+    expect(selectionSort(unsortedNegative)).to.deep.equal(sortedNegative);
+  });
+
+  it("should return an empty array if passed an empty array", function(){
+    expect(selectionSort([])).to.deep.equal([]);
+  });
+
+});
+
+
+describe("#insertionSort", function(){
+
+  it("should sort an array of positive and negative numbers ", function(){
+    expect(insertionSort(unsorted)).to.deep.equal(sorted);
+    expect(insertionSort(unsortedNegative)).to.deep.equal(sortedNegative);
+  });
+
+  it("should return an empty array if passed an empty array", function(){
+    expect(insertionSort([])).to.deep.equal([]);
+  });
+
+});
